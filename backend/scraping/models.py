d1cbdc9e09f6ca4efe39c8cd4346379385698d56
HarukiKobayashi <kobaharuharu20041008@gmail.com>
2025-05-22 10:56:50 +0900

    [add] scraping/models.pyにAssignmentモデルを追加

--- backend/scraping/models.py
+++ backend/scraping/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Assignment(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
