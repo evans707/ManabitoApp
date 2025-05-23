63b78db731474ac50c427babef88a7eb57802488
HarukiKobayashi <kobaharuharu20041008@gmail.com>
2025-05-23 18:36:00 +0900

    [add] Vue.jsとDjangoの簡単な連携

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import login_view
+from .views import Login
+from .views import SampleAPIView
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
+    path('login/', Login.as_view(), name='login'),
+    path('sample/', SampleAPIView.as_view(), name='sample'),
 ]
