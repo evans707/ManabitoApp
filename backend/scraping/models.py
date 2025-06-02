de1cbc7147c0fe9d0a1688eb7fd0a21acd4b57f3
at23114 <23rd114@ms.dendai.ac.jp>
2025-06-02 13:43:39 +0900

    [add&change] models.pyの内容を調整,マイグレーションを実行

--- backend/scraping/models.py
+++ backend/scraping/models.py
@@ -4,9 +4,13 @@ from accounts.models import User
 
 class Assignment(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
-    due_date = models.DateTimeField()
+    description = models.TextField(null=True, blank=True, help_text="課題ページのURLなどを保存") # URLを保存するために利用
+    due_date = models.DateTimeField(null=True, blank=True) # 日付が取得できない場合も考慮
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        # userとtitleの組み合わせで同じ課題の重複登録を防ぐ
+        unique_together = ('user', 'title')
 
     def __str__(self):
         return self.title
