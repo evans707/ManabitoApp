9cd501b459ee9227d2282aee93c83a6be6c6dd90
HarukiKobayashi <kobaharuharu20041008@gmail.com>
2025-06-19 10:38:49 +0900

    [change] 該当Viewのレイアウトを変更

--- frontend/src/views/CalendarView.vue
+++ frontend/src/views/CalendarView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="calendar-page">
-        <h1 class="text-2xl font-bold text-gray-800 mb-6">カレンダー</h1>
+        <h1 class="text-2xl font-semibold mb-4 text-gray-700">カレンダー</h1>
         <Card>
         <Calendar />
         </Card>
